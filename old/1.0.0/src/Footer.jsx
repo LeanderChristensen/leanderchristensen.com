2c6c94221bf1364fbe4b5bf1b5b974000d88b3c7
Leander Christensen <christensenleander@gmail.com>
2025-09-25 14:52:55 +0200

    Update email

--- old/1.0.0/src/Footer.jsx
+++ old/1.0.0/src/Footer.jsx
@@ -1,31 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Footer = () => {
-  const [showEmail, setShowEmail] = useState(false);
-  const email = 'email@l-c.io';
+    const [showEmail, setShowEmail] = useState(false);
+    const email = "email@leanderchristensen.com";
 
-  const handleClick = () => {
-    setShowEmail(true);
-  };
-  const handleClick2 = () => {
-    navigator.clipboard.writeText(email);
-    alert('Email copied to clipboard!');
-  };
+    const handleClick = () => {
+        setShowEmail(true);
+    };
+    const handleClick2 = () => {
+        navigator.clipboard.writeText(email);
+        alert("Email copied to clipboard!");
+    };
 
-  return (
-    <div id="footer">
-      {showEmail ? (
-        <p id="email" ><a onClick={handleClick2}>{email}</a></p>
-      ) : (
-        <p>
-          © Leander -{' '}
-          <a style={{"cursor": "pointer"}} onClick={handleClick}>
-            Contact Me
-          </a>
-        </p>
-      )}
-    </div>
-  );
+    return (
+        <div id="footer">
+            {showEmail ? (
+                <p id="email">
+                    <a onClick={handleClick2}>{email}</a>
+                </p>
+            ) : (
+                <p>
+                    © Leander -{" "}
+                    <a style={{ cursor: "pointer" }} onClick={handleClick}>
+                        Contact Me
+                    </a>
+                </p>
+            )}
+        </div>
+    );
 };
 
 export default Footer;
